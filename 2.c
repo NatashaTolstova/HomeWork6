9655c726b4a8f43ccd54cb99b0261573edf96030
NATASHA <klimknatasha@yandex.ru>
2024-11-12 18:11:44 +0300

    2

--- 2.c
+++ 2.c
@@ -4,3 +4,30 @@
 
 Данные на входе: Два целых числа через пробел.
 Данные на выходе: Последовательность целых чисел.*/
+
+#include <stdio.h>
+
+void printSequence(int a, int b)
+{
+    printf("%d ", a);
+    if (a > b)
+    {
+        a--;
+        printSequence(a, b);
+    }
+    else if (a < b)
+    {
+        a++;
+        printSequence(a, b);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int numberA, numberB;
+    scanf("%d %d", &numberA, &numberB);
+
+    printSequence(numberA, numberB);
+
+    return 0;
+}
