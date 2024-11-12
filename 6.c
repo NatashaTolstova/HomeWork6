3e446c26a0f590946158e80286ad04dca48cafbd
NATASHA <klimknatasha@yandex.ru>
2024-11-12 18:57:34 +0300

    6

--- 6.c
+++ 6.c
@@ -5,3 +5,25 @@ int recurs_power(int n, int p)
 
 Данные на входе: Два целых числа -100 >= n >= 100 и 0 >= p >= 100
 Данные на выходе: Одно целое число n в степени p*/
+
+#include <stdio.h>
+
+int recurs_power(int n, int p)
+{
+    if (p > 0)
+    {
+        return n * recurs_power(n, --p);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int n, p, pow;
+    scanf("%d %d", &n, &p);
+    printf("%d", recurs_power(n, p)); 
+    return 0;
+}
