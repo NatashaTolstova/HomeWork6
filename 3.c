6543089ec40c68f4b7bc54c8eb25a3d7b2d0be35
NATASHA <klimknatasha@yandex.ru>
2024-11-12 18:46:34 +0300

    3

--- 3.c
+++ 3.c
@@ -4,3 +4,24 @@
 Данные на входе: Натуральное число
 Данные на выходе: Целое число - количество единиц в двоичной записи
 числа.*/
+
+#include <stdio.h>
+
+int getOnesQuantity(int num)
+{
+    int ones = 0;
+    while (num > 0)
+    {
+        ones += num % 2;
+        num /= 2;
+    }
+    return ones;
+}
+
+int main(int argc, char const *argv[])
+{
+    int number;
+    scanf("%d", &number);
+    printf("%d", getOnesQuantity(number));
+    return 0;
+}
