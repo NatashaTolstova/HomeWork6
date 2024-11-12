a32c7ac8d42503dc53295946c01c977a28f0add4
NATASHA <klimknatasha@yandex.ru>
2024-11-12 18:51:33 +0300

    4

--- 4.c
+++ 4.c
@@ -6,3 +6,23 @@
 число 0.
 Данные на выходе: Введенная последовательность, кроме четных чисел
 в том же порядке. Число 0 не выводить.*/
+
+#include <stdio.h>
+
+void printOdds(void);
+int main()
+{
+    printOdds();
+    return 0;
+}
+
+void printOdds(void)
+{
+int a;
+    scanf("%d", &a);
+    if (a == 0)
+        return;
+    if (a % 2 != 0)
+         printf("%d ", a);
+    printOdds();
+}
